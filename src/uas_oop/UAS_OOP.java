4c7f616352defe5998c067b1c4e74ffba575db92
umardani <umardani077@gmail.com>
2018-07-08 15:12:28 +0800

    memasukkan code if

--- src/uas_oop/UAS_OOP.java
+++ src/uas_oop/UAS_OOP.java
@@ -37,6 +37,37 @@ public class UAS_OOP {
                  }else{
                      System.out.println("Waktu Keberangkatan Anda jam 16.00");
                  }
+                 System.out.println("Pembayaran   Rp "+jmh);
+             }else if(a==2){
+                 System.out.println("Pilih Waktu Keberangkatan");
+                 System.out.println("1. 08.00    2. 13.00   3. 16.00");
+                 d=no.nextInt();
+                 System.out.println("Jumlah :");
+                 c=no.nextInt();
+                 int jmh=c* 110000;
+                 if(d==1){
+                     System.out.println("Waktu Keberangkatan Anda jam 08.00");
+                 }else if(d==2){
+                     System.out.println("Waktu Keberangkatan Anda jam 13.00");
+                 }else{
+                     System.out.println("Waktu Keberangkatan Anda jam 16.00");
+                 }
+                 System.out.println("Pembayaran   Rp "+jmh);
+             }else if(a==3){
+                 System.out.println("Pilih Waktu Keberangkatan");
+                 System.out.println("1. 08.00    2. 13.00   3. 16.00");
+                 d=no.nextInt();
+                 System.out.println("Jumlah :");
+                 c=no.nextInt();
+                 int jmh=c* 200000;
+                 if(d==1){
+                     System.out.println("Waktu Keberangkatan Anda jam 08.00");
+                 }else if(d==2){
+                     System.out.println("Waktu Keberangkatan Anda jam 13.00");
+                 }else{
+                     System.out.println("Waktu Keberangkatan Anda jam 16.00");
+                 }
+                 System.out.println("Pembayaran   Rp "+jmh);
              }
               Scanner rumus = new Scanner(System.in);   
             System.out.print("Mau Memesan Lagi (Y/T) "); 
