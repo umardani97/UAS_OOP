3b7f28e7cc9719cba43bac53cf993c8f744bbfa3
umardani <umardani077@gmail.com>
2018-07-08 14:51:58 +0800

    Menu pilihan

--- src/uas_oop/UAS_OOP.java
+++ src/uas_oop/UAS_OOP.java
@@ -10,12 +10,12 @@ import java.util.Scanner;
  * @author JKendil
  */
 public class UAS_OOP {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("-----Tiket TRAVEL 21----");
+        System.out.println("1. Palangka Raya-Banjarmasin    Rp 120.000/org");
+        System.out.println("2. Palangka Raya-Sampit         Rp 110.000/org");
+        System.out.println("2. Banjarmasin-Samarinda        Rp 200.000/org");
+        System.out.println("Masukkan No Sesuai Tujuan Anda   ");
     }
     
 }
