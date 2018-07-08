b55be059f579f165ef0bf5975a8bdee01431ba31
umardani <umardani077@gmail.com>
2018-07-08 14:46:49 +0800

    mencoba

--- src/uas_oop/UAS_OOP.java
+++ src/uas_oop/UAS_OOP.java
@@ -15,7 +15,7 @@ public class UAS_OOP {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello world");
     }
     
 }
